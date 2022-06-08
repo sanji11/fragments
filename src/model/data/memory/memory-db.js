8bb8694f85c3f9dddbc3050900772eef9aa9fead
Sanjida Afrin <safrin2@myseneca.ca>
2022-06-08 13:09:04 -0400

    Update memorydb class and add new test for query()

--- src/model/data/memory/memory-db.js
+++ src/model/data/memory/memory-db.js
@@ -2,6 +2,7 @@ const validateKey = (key) => typeof key === 'string';
 
 class MemoryDB {
   constructor() {
+    /** @type {Record<string, any>} */
     this.db = {};
   }
 
@@ -9,7 +10,7 @@ class MemoryDB {
    * Gets a value for the given primaryKey and secondaryKey
    * @param {string} primaryKey
    * @param {string} secondaryKey
-   * @returns Promise<any>
+   * @returns {Promise<any>}
    */
   get(primaryKey, secondaryKey) {
     if (!(validateKey(primaryKey) && validateKey(secondaryKey))) {
@@ -27,7 +28,7 @@ class MemoryDB {
    * Puts a value into the given primaryKey and secondaryKey
    * @param {string} primaryKey
    * @param {string} secondaryKey
-   * @returns Promise
+   * @returns {Promise<void>}
    */
   put(primaryKey, secondaryKey, value) {
     if (!(validateKey(primaryKey) && validateKey(secondaryKey))) {
@@ -48,7 +49,7 @@ class MemoryDB {
    * Queries the list of values (i.e., secondaryKeys) for the given primaryKey.
    * Always returns an Array, even if no items are found.
    * @param {string} primaryKey
-   * @returns Promise<any[]>
+   * @returns {Promise<any[]>}
    */
   query(primaryKey) {
     if (!validateKey(primaryKey)) {
@@ -57,15 +58,15 @@ class MemoryDB {
 
     // No matter what, we always return an array (even if empty)
     const db = this.db;
-    const values = db[primaryKey] && Object.values(db[primaryKey]);
-    return Promise.resolve([].concat(values));
+    const values = db[primaryKey] ? Object.values(db[primaryKey]) : [];
+    return Promise.resolve(values);
   }
 
   /**
    * Deletes the value with the given primaryKey and secondaryKey
    * @param {string} primaryKey
    * @param {string} secondaryKey
-   * @returns Promise<any[]>
+   * @returns {Promise<void>}
    */
   async del(primaryKey, secondaryKey) {
     if (!(validateKey(primaryKey) && validateKey(secondaryKey))) {
