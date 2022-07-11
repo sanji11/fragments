49c2a3bbd884c0047c27caf060d1727e0d734396
Sanjida Afrin <safrin2@myseneca.ca>
2022-07-11 12:09:28 -0400

    Add GET /fragments/?expand=1 route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,13 +1,22 @@
 // src/routes/api/get.js
 
+const { Fragment } = require('../../../src/model/fragment');
+
 // Success response function
 const { createSuccessResponse } = require('../../../src/response');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working...
-  const data = { fragments: [] };
+module.exports = async (req, res) => {
+  var expand = false;
+  /* Check if the user requested expanded version of 
+  fragments (include a full representations of the fragments' metadata) or just fragments' id*/
+  if (req.query.expand === '1') {
+    expand = true;
+  }
+  // Get fragments for current user
+  const listOfFragments = await Fragment.byUser(req.user, expand);
+  const data = { fragments: listOfFragments };
   const successResponse = createSuccessResponse(data);
   res.status(200).json(successResponse);
 };
--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const { Fragment } = require('../../src/model/fragment');
+
 describe('GET /v1/fragments', () => {
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
@@ -12,13 +14,45 @@ describe('GET /v1/fragments', () => {
   test('incorrect credentials are denied', () =>
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
-  // Using a valid username/password pair should give a success result with a .fragments array
+  // Using a valid username/password pair should give a success result with a fragments array
   test('authenticated users get a fragments array', async () => {
+    // Create a test fragment
+    const postResponse = await request(app)
+      .post('/v1/fragments')
+      .auth('john@email.com', 'test@23')
+      .send('Testing text fragment')
+      .set('Content-type', 'text/plain');
+
+    // Get the fragments metadata record for the current user (non-expanded version)
+    const fragments = await Fragment.byUser(postResponse.body.fragment.ownerId, false);
+
+    // Call get /fragments to get all fragments of current user (just fragments' id)
     const res = await request(app).get('/v1/fragments').auth('john@email.com', 'test@23');
+
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
     expect(Array.isArray(res.body.fragments)).toBe(true);
+    expect(res.body.fragments).toEqual(fragments);
   });
 
-  // TODO: we'll need to add tests to check the contents of the fragments array later
+  // Using a valid username/password pair should give a success result with a fragments array with a full representations of the fragments' metadata
+  test('authenticated users get a fragments array with full fragments metadata', async () => {
+    // Create a test fragment
+    const postResponse = await request(app)
+      .post('/v1/fragments')
+      .auth('john@email.com', 'test@23')
+      .send('Testing text fragment')
+      .set('Content-type', 'text/plain');
+
+    // Get the fragments metadata record for the current user (expanded version)
+    const fragments = await Fragment.byUser(postResponse.body.fragment.ownerId, true);
+
+    // Call get /fragments to get all fragments of current user (full metadata)
+    const res = await request(app).get('/v1/fragments?expand=1').auth('john@email.com', 'test@23');
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
+    expect(Array.isArray(res.body.fragments)).toBe(true);
+    expect(res.body.fragments).toEqual(fragments);
+  });
 });
