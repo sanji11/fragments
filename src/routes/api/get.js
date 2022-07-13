c8af2c39fbc6e20ed85ae6c01d9aab31432d9327
Sanjida Afrin <safrin2@myseneca.ca>
2022-07-13 11:29:58 -0400

    Refactor code for get

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -9,17 +9,9 @@ const { createSuccessResponse } = require('../../../src/response');
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
-  var expand = false;
-  /* Check if the user requested expanded version of 
+  /* Get fragments for current user with expanded version of 
   fragments (include a full representations of the fragments' metadata) or just fragments' id*/
-  if (req.query.expand === '1') {
-    expand = true;
-  }
-  logger.debug({ expand }, 'Requested expanded version');
-  // Get fragments for current user
-  const listOfFragments = await Fragment.byUser(req.user, expand);
+  const listOfFragments = await Fragment.byUser(req.user, req.query.expand);
   logger.info({ listOfFragments }, 'Got all the fragments created by current user');
-  const data = { fragments: listOfFragments };
-  const successResponse = createSuccessResponse(data);
-  res.status(200).json(successResponse);
+  res.status(200).json(createSuccessResponse({ fragments: listOfFragments }));
 };
