379c532be49db9dd09c7f9a2e9fc150540b81cf3
Sanjida Afrin <safrin2@myseneca.ca>
2022-07-30 10:49:23 -0400

    Set up S3

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,2 +1,5 @@
-//Pick the appropriate back-end data strategy
-module.exports = require('./memory');
+// src/model/data/index.js
+
+// If the environment sets an AWS Region, we'll use AWS backend
+// services (S3, DynamoDB); otherwise, we'll use an in-memory db.
+module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
